648ca95ff61b8e4a57292b11a306878daecc7529
Andrew <ramettaandrewj@gmail.com>
2021-04-18 15:09:20 -0400

    modified config and added postgrator config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,10 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
+  JWT_SECRET: process.env.JWT_SECRET || "pay-it-forward-jwt",
+  DATABASE_URL:
+    process.env.DATABASE_URL || "postgresql://Metta@localhost/pay-it-forward",
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL ||
+    "postgresql://Metta@localhost/pay-it-forward-test",
 };

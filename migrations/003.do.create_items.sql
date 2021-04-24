334e6c2c7fbc88d71724f4acf0b2fbd7c8ab7770
Andrew <ramettaandrewj@gmail.com>
2021-04-24 09:22:34 -0400

    add items migration

---
+++ migrations/003.do.create_items.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS items;
+
+DROP TYPE IF EXISTS status;
+CREATE TYPE status as ENUM(
+    'available',
+    'claimed'
+);
+
+CREATE TABLE items (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER
+        REFERENCES users(id) ON DELETE CASCADE NOT NULL,
+    org_id INTEGER
+        REFERENCES organizations(id) ON DELETE CASCADE NOT NULL,
+    cur_status status,
+    title TEXT,
+    description TEXT,
+    photo 
+)

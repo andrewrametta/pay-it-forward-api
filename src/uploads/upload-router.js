f3c932f5411b1f97bea1f04f1338e21ae63cf9f3
Andrew <ramettaandrewj@gmail.com>
2021-05-23 16:06:07 -0400

    modifying post endpoint

--- src/uploads/upload-router.js
+++ src/uploads/upload-router.js
@@ -2,21 +2,34 @@ const express = require("express");
 const uploadRouter = express.Router();
 const { cloudinary } = require("../../utilities/cloundinary");
 
-uploadRouter.post("/", async (req, res) => {
-  try {
-    const fileString = req.body.data;
-    const uploadResponse = await cloudinary.uploader.upload(fileString, {
+// uploadRouter.post("/", async (req, res) => {
+//   try {
+//     const fileString = req.body.data;
+//     const uploadResponse = await cloudinary.uploader.upload(fileString, {
+//       upload_preset: "payitforward",
+//       timeout: 100000,
+//     });
+//     console.log(uploadResponse);
+//     console.log(uploadResponse.url);
+//     res.send(uploadResponse);
+//     console.log(fileString);
+//   } catch (error) {
+//     console.error(error);
+//     res.send(error);
+//   }
+// });
+
+uploadRouter.route("/").post((req, res, next) => {
+  const fileString = req.body.data;
+  cloudinary.uploader
+    .upload(fileString, {
       upload_preset: "payitforward",
       timeout: 100000,
-    });
-    console.log(uploadResponse);
-    console.log(uploadResponse.url);
-    res.send(uploadResponse);
-    console.log(fileString);
-  } catch (error) {
-    console.error(error);
-    res.send(error);
-  }
+    })
+    .then((img) => {
+      res.status(201).send(img);
+    })
+    .catch(next);
 });
 
 module.exports = uploadRouter;

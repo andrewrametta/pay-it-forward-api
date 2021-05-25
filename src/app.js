1311820b5d13a9bac0d0350281d7a2a89fb7156b
Andrew <ramettaandrewj@gmail.com>
2021-05-25 10:24:24 -0400

    add CLIENT ORIGIN cors

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(express.json());
 app.use(morgan(morganOption));
-app.use(cors());
+app.use(cors("*"));
 // app.use(
 //   cors({
 //     origin: CLIENT_ORIGIN,
